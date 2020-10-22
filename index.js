417da6ed11130f9b744833a9fde4e4718dbae5e7
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-10-23 00:50:47 +0530

    adding more data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var api_key = "W5Y6Jyh7Ju0wPhQ9Vw0ahIAvHIb7uKjv"
 var url_1 = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}`
 var url_2 = `https://api.giphy.com/v1/gifs/categories?api_key=${api_key}`
+var url_3 = `https://api.giphy.com/v1/stickers/trending?api_key=${api_key}`
 fetch(url_1)
 .then(res=>{
     return res.json()
@@ -18,6 +19,15 @@ fetch(url_2)
     add_random_gifs(data.data);
 })
 
+fetch(url_3)
+.then(res=>{
+    return res.json()
+})
+.then(data=>{
+    add_random_stickers(data.data);
+})
+
+
 function add_gifs(gifs){
     var gif_box = document.getElementById('gifs_id'); 
     gifs.forEach(element => {
@@ -34,6 +44,16 @@ function add_random_gifs(gifs){
         gif_box.innerHTML += 
         `<div class='gif'>
             <img src = ${element.gif.images.original.url} alt='loading'>
+        </div>`
+    });
+}
+
+function add_random_stickers(gifs){
+    var gif_box = document.getElementById('gifs_stickers'); 
+    gifs.forEach(element => {
+        gif_box.innerHTML += 
+        `<div class='gif'>
+            <img src = ${element.images.original.url} alt='loading'>
         </div>`
     });
 }
