31a6895fb5a777146ecad2482918723ee652cfff
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-11-30 15:49:17 +0530

    adding all gifs

--- JS/script.js
+++ JS/script.js
@@ -2,19 +2,30 @@ var api_key = "W5Y6Jyh7Ju0wPhQ9Vw0ahIAvHIb7uKjv";
 var url_1 = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}`;
 var url_2 = `https://api.giphy.com/v1/gifs/categories?api_key=${api_key}`;
 var url_3 = `https://api.giphy.com/v1/stickers/trending?api_key=${api_key}`;
-fetch(url_3)
-.then(res=>{
+4;
+fetch(url_1)
+  .then((res) => {
     return res.json();
-})
-.then(data=>{
+  })
+  .then((data) => {
     console.log(data);
-    add_stickers(data.data);
-})
+    add_all(data.data);
+  });
 
-function add_stickers(data){
-    data.forEach(gif =>{
-        `<div class='gif'>
-            <img src=${gif.images.original.url}/>  
-        </div>`  
-    });
+fetch(url_3)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    add_all(data.data);
+  });
+
+var gifs = document.getElementById("gifs");
+function add_all(data) {
+  data.forEach((gif) => {
+    gifs.innerHTML += `<div class='gif'>
+            <img src=${gif.images.original.webp}/>  
+        </div>`;
+  });
 }
