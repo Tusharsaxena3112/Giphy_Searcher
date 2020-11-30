a2eb249a1439b6c52e91ae1b0ffa30e230d39d6a
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-11-30 11:17:16 +0530

    adding files

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+var api_key = "W5Y6Jyh7Ju0wPhQ9Vw0ahIAvHIb7uKjv"
+var url_1 = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}`
+var url_2 = `https://api.giphy.com/v1/gifs/categories?api_key=${api_key}`
+var url_3 = `https://api.giphy.com/v1/stickers/trending?api_key=${api_key}`
