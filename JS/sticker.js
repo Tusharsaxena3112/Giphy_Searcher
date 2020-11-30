e6873452a72bc0f6189f1c7f82bd30e1e16a1dbb
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-11-30 12:54:18 +0530

    changing some stickers

--- JS/sticker.js
+++ JS/sticker.js
@@ -1,7 +1,6 @@
 var api_key = "W5Y6Jyh7Ju0wPhQ9Vw0ahIAvHIb7uKjv";
-var url_1 = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}`;
-var url_2 = `https://api.giphy.com/v1/gifs/categories?api_key=${api_key}`;
 var url_3 = `https://api.giphy.com/v1/stickers/trending?api_key=${api_key}`;
+
 fetch(url_3)
 .then(res=>{
     return res.json();
@@ -12,9 +11,11 @@ fetch(url_3)
 })
 
 function add_stickers(data){
+    var gifs = document.getElementById('gifs');
     data.forEach(gif =>{
+        gifs.innerHTML+= 
         `<div class='gif'>
             <img src=${gif.images.original.url}/>  
         </div>`  
     });
-}
+};
